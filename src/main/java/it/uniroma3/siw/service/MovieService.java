3a9d1c153f60ce370e8337faf77d01b94bdf8003
Chiodoo <marco.chiodo03@gmail.com>
2025-03-13 09:10:59 +0100

    Aggiunta classe MovieService per farci restituire tutti i Movie o solo uno dall'id

---
+++ src/main/java/it/uniroma3/siw/service/MovieService.java
@@ -0,0 +1,21 @@
+package it.uniroma3.siw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import it.uniroma3.siw.model.Movie;
+import it.uniroma3.siw.repository.MovieRepository;
+
+@Service
+public class MovieService {
+
+    @Autowired  //Fa in modo di instanziare un oggetto di una classe che implementa questa interfaccia, in modo che non ci pensi io (lo fa il framework)
+    private MovieRepository movieRepository;
+
+    public Movie getMovieById(long id) {
+        return movieRepository.findById(id).get();
+    }
+
+    public Iterable<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+}
