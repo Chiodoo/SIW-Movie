e21c52fc3fd229bea37a6563fe9c95101fa81494
Chiodoo <marco.chiodo03@gmail.com>
2025-03-13 09:49:01 +0100

    Aggiunta classe MovieController() per gestire il mapping tra richieste (solo richieste GET per ora)  HTTP e i metodi del mio programma

---
+++ src/main/java/it/uniroma3/siw/controller/MovieController.java
@@ -0,0 +1,36 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import it.uniroma3.siw.service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MovieController {
+
+    @Autowired MovieService movieService;       //Mi crei l'oggetto e me lo metti a disposizione automaticamente
+
+
+    //Metto a disposizione di chi genera la risposta un metodo (secondo parametro) per ricevere la risposta, 
+    //che metto dentro alla variabile "movie" (o "movies") che passo come primo parametro
+    
+    @GetMapping("/movie/{id}")
+    public String getMovie(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("movie", this.movieService.getMovieById(id));    
+        return "movie.html";
+    }
+
+    @GetMapping("/movie")
+    public String showMovies(Model model) {
+        model.addAttribute("movies", this.movieService.getAllMovies());
+        return "movies.html";
+    }
+    
+
+
+
+}
